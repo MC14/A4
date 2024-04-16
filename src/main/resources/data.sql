fbd6727815bdbf276a12e0d284cf6b0b306f234a
Morgan Cowans <cowansm@sheridancollege.ca>
2024-04-16 18:27:31 -0400

    finalizing data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO COURSE(COURSENAME,COURSECODE,COURSECREDIT,EDIT)
-VALUES ('Intro to Programming','PROG1234','22','false'),
-       ('Drama Class','DRM2301','33','false'),
-       ('Calculus','MATH1101','421','false');
+INSERT INTO COURSE(COURSEID,COURSENAME,COURSECODE,COURSECREDIT,EDIT)
+VALUES (1223,'Intro to Programming','PROG1234','22','false'),
+       (1356,'Drama Class','DRM2301','33','false'),
+       (2345,'Calculus','MATH1101','421','false');
